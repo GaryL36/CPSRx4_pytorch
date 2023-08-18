36413c566e7bd2e6e3461dd3750521ab13542603
GrayLeung36 <73875613+GrayLeung36@users.noreply.github.com>
2023-08-18 11:32:08 +0800

    Add files via upload

---
+++ pydct/__init__.py
@@ -0,0 +1,27 @@
+"""
+Fast Discrete Curvelet Transform
+
+Uses libraries from CurveLab 2.0 
+
+developed by:
+Emmanuel Candes, Laurent Demanet, and Lexing Ying
+California Institute of Technology
+
+this wrapper developed by:
+Darren Thomson and Gilles Hennenfent
+Seismic Laboratory for Imaging and Modeling
+University of British Columbia
+"""
+
+# This software was written by Darren Thomson and Gilles Hennenfent.
+# Copyright owned by The University of British Columbia, 2006.  This
+# software is distributed and certain rights granted under a License
+# Agreement with The University of British Columbia. Please contact
+# the authors or the UBC University Industry Liaison Office at
+# 604-822-8580 for further information.
+
+from .fdct2 import fdct2
+
+from .fdct3 import fdct3
+
+from .test import test, normtest
